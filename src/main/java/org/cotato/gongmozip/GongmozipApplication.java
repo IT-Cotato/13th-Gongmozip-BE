271f14a84db6e0a3d85d14dd1902445fc3a87da5
hamtorygoals <yhaemin0531@naver.com>
2026-06-24 22:54:29 +0900

    :tada: start: Project Gongmozip

---
+++ src/main/java/org/cotato/gongmozip/GongmozipApplication.java
@@ -0,0 +1,13 @@
+package org.cotato.gongmozip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongmozipApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GongmozipApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/cotato/gongmozip/GongmozipApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cotato.gongmozip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongmozipApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
