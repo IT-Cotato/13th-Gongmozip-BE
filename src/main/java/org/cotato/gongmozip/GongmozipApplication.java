0703eb9cbb7b40346af626ae6adff761916e1454
hamtorygoals <yhaemin0531@naver.com>
2026-06-24 23:05:48 +0900

    :art: style: Spotless Palantir 포매터 적용

--- src/main/java/org/cotato/gongmozip/GongmozipApplication.java
+++ src/main/java/org/cotato/gongmozip/GongmozipApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GongmozipApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(GongmozipApplication.class, args);
-  }
-
+    public static void main(String[] args) {
+        SpringApplication.run(GongmozipApplication.class, args);
+    }
 }
