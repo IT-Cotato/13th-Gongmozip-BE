f59a88e4f13790157412d282416df6f84216651d
김민규 <alsrb0332@naver.com>
2026-07-06 17:09:07 +0900

    :sparkles: feat: 사용자 이메일 존재 확인 쿼리 추가

--- src/main/java/org/cotato/gongmozip/domains/member/repository/MemberRepository.java
+++ src/main/java/org/cotato/gongmozip/domains/member/repository/MemberRepository.java
@@ -4,4 +4,6 @@ import org.cotato.gongmozip.domains.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
 }
