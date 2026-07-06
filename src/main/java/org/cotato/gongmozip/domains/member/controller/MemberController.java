4b365ef4a96610d34957ea7de96b3ce05ea0adc6
김민규 <alsrb0332@naver.com>
2026-07-06 17:43:39 +0900

    :sparkles: feat: 이메일 인증 관련 Controller 추가

---
+++ src/main/java/org/cotato/gongmozip/domains/member/controller/MemberController.java
@@ -0,0 +1,46 @@
+package org.cotato.gongmozip.domains.member.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.cotato.gongmozip.domains.member.dto.request.MemberAuthRequest.EmailVerifyConfirmRequest;
+import org.cotato.gongmozip.domains.member.dto.request.MemberAuthRequest.EmailVerifyRequest;
+import org.cotato.gongmozip.domains.member.exception.codes.MemberErrorCode;
+import org.cotato.gongmozip.domains.member.exception.codes.MemberSuccessCode;
+import org.cotato.gongmozip.domains.member.service.MemberService;
+import org.cotato.gongmozip.global.exception.GlobalErrorCode;
+import org.cotato.gongmozip.global.response.BaseResponse;
+import org.cotato.gongmozip.global.response.BaseResponseFormatter;
+import org.cotato.gongmozip.global.swagger.CustomErrorCodes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Member", description = "회원 관련 API")
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Operation(summary = "이메일 인증코드 발송")
+    @CustomErrorCodes(commonErrorCodes = GlobalErrorCode.class, domainErrorCodes = MemberErrorCode.class)
+    @PostMapping("/email/verify-request")
+    public ResponseEntity<BaseResponse<Void>> sendVerificationCode(@RequestBody @Valid EmailVerifyRequest request) {
+        memberService.sendVerificationCode(request);
+        return BaseResponseFormatter.success(MemberSuccessCode.EMAIL_VERIFY_CODE_SENT);
+    }
+
+    @Operation(summary = "이메일 인증코드 확인")
+    @CustomErrorCodes(commonErrorCodes = GlobalErrorCode.class, domainErrorCodes = MemberErrorCode.class)
+    @PostMapping("/email/verify")
+    public ResponseEntity<BaseResponse<Void>> confirmVerificationCode(
+            @RequestBody @Valid EmailVerifyConfirmRequest request) {
+        memberService.confirmVerificationCode(request);
+        return BaseResponseFormatter.success(MemberSuccessCode.EMAIL_VERIFY_SUCCESS);
+    }
+}
