5f8ec77fff13d2043b11b0e2a075e91d76badade
김민규 <alsrb0332@naver.com>
2026-07-06 18:13:04 +0900

    :sparkles: feat: 회원가입 관련 DTO 추가

--- src/main/java/org/cotato/gongmozip/domains/member/dto/request/MemberAuthRequest.java
+++ src/main/java/org/cotato/gongmozip/domains/member/dto/request/MemberAuthRequest.java
@@ -2,6 +2,9 @@ package org.cotato.gongmozip.domains.member.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import org.cotato.gongmozip.domains.member.enums.Gender;
 
 public class MemberAuthRequest {
 
@@ -10,4 +13,11 @@ public class MemberAuthRequest {
 
     // 이메일 인증코드 확인 요청
     public record EmailVerifyConfirmRequest(@Email @NotBlank String email, @NotBlank String code) {}
+
+    // 회원가입 요청
+    public record SignUpRequest(
+            @Email @NotBlank String email,
+            @NotBlank String password,
+            @NotNull Gender gender,
+            @NotNull LocalDate birthDate) {}
 }
