060f56231582a5da50266cf88a84e10735f6dade
김민규 <alsrb0332@naver.com>
2026-07-07 16:15:43 +0900

    :sparkles: feat: 인증 관련 예외 및 코드 추가

---
+++ src/main/java/org/cotato/gongmozip/domains/auth/exception/codes/AuthErrorCode.java
@@ -0,0 +1,18 @@
+package org.cotato.gongmozip.domains.auth.exception.codes;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.cotato.gongmozip.global.exception.BaseErrorCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthErrorCode implements BaseErrorCode {
+
+    // 401
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "AUTH_401_1", "비밀번호가 올바르지 않습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
---
+++ src/main/java/org/cotato/gongmozip/domains/auth/exception/codes/AuthSuccessCode.java
@@ -0,0 +1,18 @@
+package org.cotato.gongmozip.domains.auth.exception.codes;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.cotato.gongmozip.global.response.BaseSuccessCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthSuccessCode implements BaseSuccessCode {
+
+    // 200
+    LOGIN_SUCCESS(HttpStatus.OK, "AUTH_200_1", "로그인에 성공하였습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
