c07d3138a6ef48b0cfff7cce4a6158162d0133cc
김민규 <alsrb0332@naver.com>
2026-07-07 18:48:40 +0900

    :sparkles: feat: Swagger JWT Bearer 인증 설정 추가

--- src/main/java/org/cotato/gongmozip/global/swagger/SwaggerConfig.java
+++ src/main/java/org/cotato/gongmozip/global/swagger/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package org.cotato.gongmozip.global.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +14,25 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        Info info = new Info().title("공모집").description("공모집 서비스 API 문서").version("0.1.0");
+
+        // JWT 토큰 헤더 방식
+        String securityScheme = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
+
+        Components components = new Components()
+                .addSecuritySchemes(
+                        securityScheme,
+                        new SecurityScheme()
+                                .name(securityScheme)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("Bearer")
+                                .bearerFormat("JWT"));
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("공모집 API")
-                        .description("공모집 서비스 API 문서")
-                        .version("v1.0.0")
-                        .contact(new Contact().name("Cotato").email("cotato@example.com")));
+                .info(info)
+                .addServersItem(new Server().url("http://localhost:8080").description("로컬"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
