a75cc677fbb2079d498d1bc7512a6f52d7b930ba
chaeyylee <ivystar1129@naver.com>
2026-07-17 02:42:11 +0900

    🔧 chore: CertificationSeeder 및 example 패키지 삭제

--- src/main/java/org/cotato/gongmozip/domains/profile/service/CertificationSeeder.java
+++
@@ -1,9 +0,0 @@
-package org.cotato.gongmozip.domains.profile.service;
-
-/**
- * 자격증 초기 데이터 시딩은 Flyway 마이그레이션(V1__init_certifications.sql)을 통해 처리됩니다.
- * 기동 시 CommandLineRunner 기반 시딩 로직은 제거되었습니다.
- */
-public class CertificationSeeder {
-    private CertificationSeeder() {}
-}
