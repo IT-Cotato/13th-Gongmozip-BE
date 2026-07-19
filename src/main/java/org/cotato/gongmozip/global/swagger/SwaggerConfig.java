9247574ae29e924ddc8b344c07f599b4f77f6771
김민규 <alsrb0332@naver.com>
2026-07-19 20:43:48 +0900

    :wrench: chore: Swagger 서버 목록에 배포 서버 URL 추가

--- src/main/java/org/cotato/gongmozip/global/swagger/SwaggerConfig.java
+++ src/main/java/org/cotato/gongmozip/global/swagger/SwaggerConfig.java
@@ -31,6 +31,7 @@ public class SwaggerConfig {
 
         return new OpenAPI()
                 .info(info)
+                .addServersItem(new Server().url("https://13.209.254.149.nip.io").description("배포 서버"))
                 .addServersItem(new Server().url("http://localhost:8080").description("로컬"))
                 .addSecurityItem(securityRequirement)
                 .components(components);
