4a8434392f049989d6505db268b4008e05718b25
김민규 <alsrb0332@naver.com>
2026-07-25 00:57:52 +0900

    :wrench: fix: H2 호환을 위해 V4 동적 SQL 제거 및 V6 COMMENT 구문 제거

--- src/main/resources/db/migration/V4__add_contest_unique_constraint.sql
+++ src/main/resources/db/migration/V4__add_contest_unique_constraint.sql
@@ -1,4 +1,2 @@
 -- contest 테이블에 (title, apply_end_at) 유니크 제약조건 추가
--- 멱등 처리: 이미 존재하는 경우 DROP 후 재추가
-ALTER TABLE contest DROP INDEX IF EXISTS uq_contest_title_apply_end_at;
 ALTER TABLE contest ADD CONSTRAINT uq_contest_title_apply_end_at UNIQUE (title, apply_end_at);
