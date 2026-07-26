1b1f1f4b1b3d36fd0a909e8413a06cf36671a0b7
김민규 <alsrb0332@naver.com>
2026-07-26 21:59:47 +0900

    :zap: perf: 설문 답변 삭제 N+1 문제 개선

--- src/main/java/org/cotato/gongmozip/domains/survey/repository/SurveyAnswerRepository.java
+++ src/main/java/org/cotato/gongmozip/domains/survey/repository/SurveyAnswerRepository.java
@@ -4,10 +4,14 @@ import java.util.List;
 import org.cotato.gongmozip.domains.survey.entity.SurveyAnswer;
 import org.cotato.gongmozip.domains.survey.entity.SurveySubmission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SurveyAnswerRepository extends JpaRepository<SurveyAnswer, Long> {
 
     List<SurveyAnswer> findBySubmission(SurveySubmission submission);
 
+    @Modifying
+    @Query("DELETE FROM SurveyAnswer sa WHERE sa.submission = :submission")
     void deleteAllBySubmission(SurveySubmission submission);
 }
