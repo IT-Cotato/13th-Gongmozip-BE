02f7951f95333a2c28e6ef3eb77e16e30f94457e
김민규 <alsrb0332@naver.com>
2026-08-06 09:32:20 +0900

    :sparkles: feat: 문의 상태별 페이징 조회 레포지토리 메서드 추가

--- src/main/java/org/cotato/gongmozip/domains/inquiry/repository/InquiryRepository.java
+++ src/main/java/org/cotato/gongmozip/domains/inquiry/repository/InquiryRepository.java
@@ -2,10 +2,15 @@ package org.cotato.gongmozip.domains.inquiry.repository;
 
 import java.util.List;
 import org.cotato.gongmozip.domains.inquiry.entity.Inquiry;
+import org.cotato.gongmozip.domains.inquiry.enums.InquiryStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
 
     // 비밀번호 대조가 애플리케이션에서 이뤄지므로 조회 행 수를 제한해 BCrypt 연산 폭증(DoS)을 방지한다
     List<Inquiry> findTop100ByEmailOrderByCreatedAtDesc(String email);
+
+    Page<Inquiry> findAllByStatus(InquiryStatus status, Pageable pageable);
 }
