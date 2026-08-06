5249f485fe3917c8a24ec7e9bb7e8772dc215b84
김민규 <alsrb0332@naver.com>
2026-08-06 09:32:12 +0900

    :sparkles: feat: 관리자 문의 조회/답변 에러·성공 코드 추가

--- src/main/java/org/cotato/gongmozip/domains/inquiry/exception/codes/InquiryErrorCode.java
+++ src/main/java/org/cotato/gongmozip/domains/inquiry/exception/codes/InquiryErrorCode.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum InquiryErrorCode implements BaseErrorCode {
 
+    // 400
+    INQUIRY_INVALID_INPUT(HttpStatus.BAD_REQUEST, "INQUIRY_400_1", "유효하지 않은 요청 값입니다."),
+
     // 404
     INQUIRY_NOT_FOUND(HttpStatus.NOT_FOUND, "INQUIRY_404_1", "해당 정보로 접수된 문의가 없습니다. 메일 또는 비밀번호를 다시 확인해 주세요."),
     ;
